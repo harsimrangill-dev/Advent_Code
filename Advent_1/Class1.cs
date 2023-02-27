6d22990ce77570c2633ce84e62ac41ef28597c49
harsimrangill <parthsharma12341@gmail.com>
2023-02-27 22:19:30 +0000

    this is the first day

---
+++ Advent_1/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+public class Class1
+{
+    string[] inputFIle = File.ReadAllLines("input.txt");
+    int  max = 0;
+    int elfNo = 1;
+    
+    public int getLength()
+    {
+        int count = 0;
+        foreach( string str in inputFIle)
+        {
+            if (str == "")
+            {
+                count++;
+            }
+        }
+        return count + 1;
+    }
+    public void maxVal()
+    {
+        int i = 0;
+        int sum = 0;
+        foreach (string str in inputFIle)
+        {
+            if (str != "")
+            {
+                int num = Convert.ToInt32(str);
+                sum += num;
+            }
+            else if (str == "" || i ==  getLength () - 1) 
+            {
+                elfNo++;
+                if (max < sum)
+                {
+                    max = sum;
+                }
+                sum = 0;
+            }
+            i++;
+
+        }
+        
+        Console.WriteLine(elfNo + "th elf has food with maximum calories:  " + max);
+        
+    }
+
+    public int[] intarray()
+    {
+        int len = this.getLength();
+        int[] cals = new int[len];
+        int count = 0;
+        int i = 0;
+        int sum = 0;
+
+        foreach (string str in inputFIle)
+        {
+            if (str != "")
+            {
+                int num = Convert.ToInt32(str);
+                sum+= num;
+            }
+            if (str == "" || i == inputFIle.Length - 1 )
+            {
+                cals[count] = sum;
+                count++;
+                sum = 0;
+                
+            }
+            i++;
+
+        }
+        return cals;
+    }
+    
+}
---
+++ Advent_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System.IO;
+using System.Threading.Tasks.Dataflow;
+
+namespace Advent_code
+{
+    class codes
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+
+            obj.maxVal();
+
+            int[] calories = new int[obj.getLength()];
+
+            calories = obj.intarray();
+
+            Array.Sort(calories);
+
+            int  total;
+
+            total = calories[obj.getLength() - 1] + calories[obj.getLength() - 2] + calories[obj.getLength() - 3];
+            
+            Console.WriteLine(total);
+
+
+            
+
+        }
+    }
+}
